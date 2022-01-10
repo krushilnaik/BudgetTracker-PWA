0ef0b77958e35f54371846395d4226d3d4125af8
Krushil Naik <krushilnaik96@gmail.com>
2022-01-09 19:42:56 -0500

    Update service worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -42,13 +42,41 @@ self.addEventListener("activate", function (event) {
 	);
 });
 
-self.addEventListener("fetch", function (event) {
-	event.respondWith(
-		caches.match(event.request).then(function (request) {
-			if (request) {
-				return request;
-			} else {
-				return fetch(event.request);
+// Intercept fetch requests
+self.addEventListener("fetch", function (evt) {
+	if (evt.request.url.includes("/api/")) {
+		evt.respondWith(
+			caches
+				.open(DATA_CACHE_NAME)
+				.then((cache) => {
+					return fetch(evt.request)
+						.then((response) => {
+							// If the response was good, clone it and store it in the cache.
+							if (response.status === 200) {
+								cache.put(evt.request.url, response.clone());
+							}
+
+							return response;
+						})
+						.catch((err) => {
+							// Network request failed, try to get it from the cache.
+							return cache.match(evt.request);
+						});
+				})
+				.catch((err) => console.log(err))
+		);
+
+		return;
+	}
+
+	evt.respondWith(
+		fetch(evt.request).catch(async function () {
+			const response = await caches.match(evt.request);
+			if (response) {
+				return response;
+			} else if (evt.request.headers.get("accept").includes("text/html")) {
+				// return the cached home page for all requests for html pages
+				return caches.match("/");
 			}
 		})
 	);
