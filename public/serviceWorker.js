dede04c245edfb0e72da6ef7bdd9745b0593c23f
Krushil Naik <krushilnaik96@gmail.com>
2022-01-09 19:21:54 -0500

    Update service worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -17,73 +17,39 @@ const FILES_TO_CACHE = [
 	"./icons/icon-72x72.png"
 ];
 
-// Install the service worker
-self.addEventListener("install", function (evt) {
-	evt.waitUntil(
-		caches.open(CACHE_NAME).then((cache) => {
-			console.log("Your files were pre-cached successfully!");
-			return cache.addAll(FILES_TO_CACHE);
-		})
+self.addEventListener("install", function (event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
 	);
-
-	self.skipWaiting();
 });
 
-// Activate the service worker and remove old data from the cache
-self.addEventListener("activate", function (evt) {
-	evt.waitUntil(
-		caches.keys().then((keyList) =>
-			Promise.all(
-				keyList.map((key) => {
-					if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-						console.log("Removing old cache data", key);
-						return caches.delete(key);
+self.addEventListener("activate", function (event) {
+	event.waitUntil(
+		caches.keys().then(function (keyList) {
+			let cacheKeeplist = keyList.filter(function (key) {
+				return key.indexOf(APP_PREFIX);
+			});
+			cacheKeeplist.push(CACHE_NAME);
+
+			return Promise.all(
+				keyList.map(function (key, i) {
+					if (cacheKeeplist.indexOf(key) === -1) {
+						return caches.delete(keyList[i]);
 					}
 				})
-			)
-		)
+			);
+		})
 	);
-
-	self.clients.claim();
 });
 
-// Intercept fetch requests
-self.addEventListener("fetch", function (evt) {
-	if (evt.request.url.includes("/api/")) {
-		evt.respondWith(
-			caches
-				.open(DATA_CACHE_NAME)
-				.then((cache) => {
-					return (
-						fetch(evt.request)
-							.then((response) => {
-								// If the response was good, clone it and store it in the cache.
-								if (response.status === 200) {
-									cache.put(evt.request.url, response.clone());
-								}
-
-								return response;
-							})
-							// Network request failed, try to get it from the cache.
-							.catch(() => cache.match(evt.request))
-					);
-				})
-				.catch(console.error)
-		);
-
-		return;
-	}
-
-	evt.respondWith(
-		fetch(evt.request).catch(function () {
-			return caches.match(evt.request).then((response) => {
-				if (response) {
-					return response;
-				} else if (evt.request.headers.get("accept").includes("text/html")) {
-					// return the cached home page for all requests for html pages
-					return caches.match("/");
-				}
-			});
+self.addEventListener("fetch", function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (request) {
+			if (request) {
+				return request;
+			} else {
+				return fetch(event.request);
+			}
 		})
 	);
 });
